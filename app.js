c80f2618236b83be4496cf49bbb3f9a8fcc2dfdd
mmendoza89 <mardoza21@gmail.com>
2021-01-06 09:35:43 -0300

    agregue los llamados app.get de login y register a la estructura de express y movi todos los html a la carpeta views

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ app.listen(3000, (req, res)=>{
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, ("./views/index.html")));
 });
+
+app.get("/login", (req,res)=>{
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/productCart", (req,res)=>{
+    res.sendFile(__dirname + "/views/productCart.html");
+});
+
+app.get("/productDetail",(req,res)=>{
+    res.sendFile(__dirname + "/views/productDetail.html");
+});
