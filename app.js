4e1ddd8267a7584a5c6560dc6dee4d1cc5bd8849
mmendoza89 <mardoza@Marcos-MBP.fibertel.com.ar>
2021-03-28 22:14:09 -0300

    adding register and login process

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // ************ Require's ************
 const express = require("express");
+const session = require('express-session');
+const cookies = require('cookie-parser');
 const path = require('path');
-const indexRoutes = require('./routes/mainRoutes');
 const methodOverride =  require('method-override');
 
 // ************ express() ************
 const app = express();
 
+app.use(cookies());
+
+const userLoggedMiddleware = require('./middleware/userLoggedMiddleware');
+
+app.use(session({
+	secret: "Shhh, It's a secret",
+	resave: false,
+	saveUninitialized: false,
+}));
+
+app.use(userLoggedMiddleware);
+
 // ************ Middleware ************
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-// app.use(express.static(path.join(__dirname, '../public')));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -19,26 +31,23 @@ app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
-// Mapping the EJS template engine to ".html" files (para que poder renderizar HTMLs además de ejs)
+// Mapping the EJS template engine to ".html" files
 app.engine('html', require('ejs').renderFile);
 
-const loginRouter = require('./routes/loginRoutes'); // Rutas /login
-const registerRouter = require('./routes/registerRoutes'); // Rutas /register
-const recoverPassRouter = require('./routes/recoverPassRoutes'); // Rutas /recoverPassword
+const recoverPassRouter = require(path.join(__dirname,'./routes/recoverPassRoutes'));
 const productCartRouter = require(path.join(__dirname, './routes/productCart'));
-// const productDetailRouter = require(path.join(__dirname, './routes/productDetail'));
+const productDetailRouter = require(path.join(__dirname, './routes/productDetail'));
 const productCreateRouter = require(path.join(__dirname, './routes/productCreate'));
 const productEditRouter = require(path.join(__dirname, './routes/productEdit'));
 const productsRouter = require(path.join(__dirname, './routes/products'));
-const indexRouter = require(path.join(__dirname, './routes/indexRoutes.js')); //ruta index regular, provisoria, ya sabemos que está el mainRouter
-
+const indexRouter = require(path.join(__dirname, './routes/indexRoutes'));
+const userRouter = require(path.join(__dirname, './routes/userRoutes'));
 
 app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
+app.use('/users', userRouter);
 app.use('/recoverPassword', recoverPassRouter);
 app.use('/productCart', productCartRouter);
-// app.use('/productDetail', productDetailRouter);
+app.use('/productDetail', productDetailRouter);
 app.use('/productCreate', productCreateRouter);
 app.use('/productEdit', productEditRouter);
 app.use('/products', productsRouter);
---
+++ routes/userRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+//Controller
+const userController = require(path.join(__dirname, '../controllers/userController'));
+
+//Middleware
+const uploadFile = require('../middleware/multerMiddleware')
+const validations = require('../middleware/validateRegisterMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
+const guestMiddleware = require('../middleware/guestMiddleware');
+
+
+//Register form
+router.get('/register', guestMiddleware, userController.register);
+
+// Procesar el registro
+router.post('/register', uploadFile.single('avatar'), validations, userController.processRegister);
+
+// Formulario de login
+router.get('/login', guestMiddleware, userController.login);
+
+// Procesar login
+router.post('/login', userController.loginProcess);
+
+// Perfil de Usuario
+router.get('/profile/', authMiddleware, userController.profile);
+
+// Logout
+router.get('/logout/', userController.logout);
+
+module.exports = router;
